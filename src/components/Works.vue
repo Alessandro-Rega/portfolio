d69209818634c0bb8e0117180b8f8a6d8863c60a
Alessandro-Rega <regaale47@gmail.com>
2022-01-14 23:31:05 +0100

    aggiunta settaggio media query

--- src/components/Works.vue
+++ src/components/Works.vue
@@ -13,4 +13,9 @@ export default {
 <style lang="scss" scoped>
 // Importazione variabili
 @import "../assets/style/variables.scss";
+
+@media screen and (min-width: 768px){}
+@media screen and (min-width: 1024px){}
+@media screen and (min-width: 1440px){}
+@media screen and (min-width: 1920px){}
 </style>
