663c7d1947b033c8eb17b792bda5045670316732
Alessandro-Rega <regaale47@gmail.com>
2022-01-14 19:06:30 +0100

    Settaggio componenti

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
+    <Home/>
+    <About/>
+    <Works/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+import Home from "./components/Home.vue";
+import About from "./components/About.vue";
+import Works from "./components/Works.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Home,
+    About,
+    Works,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./assets/style/global.scss";
 </style>
