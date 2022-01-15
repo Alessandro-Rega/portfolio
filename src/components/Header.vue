bf0d905168110eb87a837b6a00eb5e66bc371dc4
Alessandro-Rega <regaale47@gmail.com>
2022-01-15 13:11:20 +0100

    test ancora

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,9 +6,9 @@
       </div>
       <div class="classic-menu">
           <ul>
-              <li><a href="#">Home</a></li>
-              <li><a href="#">About</a></li>
-              <li><a href="#">Works</a></li>
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#works">Works</a></li>
           </ul>
       </div>
 
